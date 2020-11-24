03bf7d00b2f1e57d247191a2226f412c231f722b
Helena <helenajuliana14@gmail.com>
2020-11-24 20:00:01 -0300

    Ajustando o pipes da tabela telefone

--- src/app/shared/pipes/telefone.pipe.ts
+++ src/app/shared/pipes/telefone.pipe.ts
@@ -5,18 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TelefonePipe implements PipeTransform {
   transform(value: string, ): string {
-    if (value && value.length === 10  ) {
-      return `${value.substr(0, 0)}(${value.substr(0, 2)})${value.substr(3, 4)}-${value.substr(5, 4)}`;
-    }else if (value && value.length === 8) {
-      return `${value.substr(0, 4)}-${value.substr(3, 4)}`;
-    }else if (value.length === 11){
-      return `(00${value.substr(0, 0)}${value.substr(0, 0)})${value.substr(3, 4)}-${value.substr(5, 4)}`;
-
-    }
-    else{
+    if (value && value.length === 8) {
+      return `${value.substr(0, 4)}-${value.substr(4, 4)}`;
+    } else if (value && value.length === 10) {
+      return `(${value.substr(0, 2)}) ${value.substr(2, 4)}-${value.substr(6, 4)}`;
+    } else if (value && value.length === 11) {
+      return `(${value.substr(0, 3)}) ${value.substr(3, 4)}-${value.substr(7, 4)}`;
+    } else {
       return value;
     }
   }
-
-
 }
